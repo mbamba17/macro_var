69a9a2eb7b937d8baa581d67c7e879324864a11d
Mario Bambulovic <mario.bambulovic17@gmail.com>
2020-03-22 20:59:40 +0100

    draft verzija modela sa bdp-om, inflacijom, nezaposlenosti, plaćama i egzogenom varijablom eu_bdp-om

--- radni.R
+++ radni.R
@@ -1,8 +1,9 @@
-library(tidyverse)
-library(readxl)
 library(eurostat)
 library(ecb)
 library(lubridate)
+library(vars)
+library(tidyverse)
+library(readxl)
 rm(list=ls())
 
 # Kopiranje dataframea u excel
@@ -14,27 +15,40 @@ write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
 
 #### 1. Dohvat podataka ####
 
-# 1.1. Realni BDP HR ####
-bdp <- get_eurostat(id="namq_10_gdp") %>% filter(s_adj=="SCA" & na_item=="B1GQ" & unit=="CLV10_MNAC" & geo=="HR") %>% select(time,values) %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,bdp=values)
+# 1.1. Realni BDP HR
+bdp <- get_eurostat(id="namq_10_gdp") %>% filter(s_adj=="SCA" & na_item=="B1GQ" & unit=="CLV10_MNAC" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,bdp=values) %>% arrange(datum) %>% mutate(bdp=(bdp/lag(bdp,4)-1)*100) %>% na.omit()
 
-# %>% arrange(geo,time) %>% group_by(geo) %>% mutate(bdp_y=(values+lag(values,1)+lag(values,2)+lag(values,3))) %>% group_by(geo) %>% mutate(dbdp = (bdp_y/lag(bdp_y,4)-1)*100)  %>% select(datum,geo,dbdp) %>% na.omit()
-# ggplot(dbdp,aes(x=datum,y=dbdp))+geom_line()+facet_wrap(~geo,scales="free")+geom_hline(aes(yintercept=0),linetype="dashed",color="red")
+# 1.2. Realni BDP EU 
+bdp_eu <- get_eurostat(id="namq_10_gdp") %>% filter(s_adj=="SCA" & na_item=="B1GQ" & unit=="CLV10_MNAC" & geo=="EU28") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,bdp_eu=values) %>% arrange(datum) %>% mutate(bdp_eu=(bdp_eu/lag(bdp_eu,4)-1)*100) %>% na.omit()
 
-# 1.2. Realni BDP EU #### 
-bdp_eu <- get_eurostat(id="namq_10_gdp") %>% filter(s_adj=="SCA" & na_item=="B1GQ" & unit=="CLV10_MNAC" & geo=="EU28") %>% select(time,values) %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,bdp_eu=values)
-
-# 1.3. Stopa nezaposlenosti ####
+# 1.3. Stopa nezaposlenosti
 unemp <- get_eurostat(id="une_rt_q") %>% filter(s_adj=="SA" & age=="TOTAL" & sex=="T" & unit=="PC_ACT" & geo=="HR") %>% select(time,values) %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,unemp=values)
 
-
-# 1.4. Inflacija ####
+# 1.4. Inflacija
 infl <- get_eurostat(id="prc_hicp_manr") %>% filter(coicop=="CP00" & unit=="RCH_A" & geo=="HR") %>% select(time,values) %>% mutate(datum = ceiling_date(time,"month")-1) %>% filter(month(datum) %in% c(3,6,9,12)) %>% select(datum,infl=values)
 
+# 1.5. Realne bruto plaće
+wage <- read_excel("podaci.xlsx") %>% mutate(datum=as.Date(datum)) %>% filter(month(datum) %in% c(3,6,9,12)) %>% arrange(datum) %>% mutate(wage=(wage/lag(wage,4)-1)*100) %>% na.omit()
 
-# 1.5. Realne bruto plaće ####
-wage <- read_excel("podaci.xlsx") %>% mutate(datum=as.Date(datum)) %>% filter(month(datum) %in% c(3,6,9,12))
+# 1.6. Sklapanje podataka
+podaci <- inner_join(bdp,bdp_eu,by="datum") %>% inner_join(infl,by="datum") %>% inner_join(unemp,by="datum") %>% inner_join(wage,by="datum")
+egzogene <- ts(podaci[,3],end = c(2019,4),frequency=4)
+podaci <- ts(podaci[,-c(1,3)],end = c(2019,4),frequency=4)
+rm(bdp,bdp_eu,infl,unemp,wage)
 
+# Procjena unrestricted VAR modela, a tu je prvi korak određivanje broja lagova (oni su procjenjivali i za 1 i za 2 i za 3 laga modele, no mi ćemo gledati primjer sa samo 1 lagom). 
+VARselect(podaci, lag.max = 4, type = "none",exogen = egzogene)
+# procjena var modela
+var_mdl <- VAR(podaci, p = 2, type = "none", exogen = egzogene)
+var_mdl # ovo nam daje koeficijente svih jednadžbi
+summary(var_mdl, equation = "bdp") # daje ključne podatke o jdbi za dbdp
+summary(var_mdl, equation = "infl")
+summary(var_mdl, equation = "unemp")
+summary(var_mdl, equation = "wage")
 
-# Sklapanje panelice za model yielda
-panel = inner_join(yld,us_yld,by="datum") %>% inner_join(pdebt,by=c("datum","geo")) %>% inner_join(pdeficit,by=c("datum","geo")) %>% inner_join(ciss_bond,by="datum") %>% inner_join(euribor,by="datum") %>% inner_join(dbdp,by=c("datum","geo")) %>% inner_join(cabalance,by=c("datum","geo")) %>% inner_join(investicijski,by=c("datum","geo")) %>% na.omit() %>% filter(geo!="EU28")
+# na kraju možemo plotati fit modela
+plot(var_mdl) # svi zajedno
 
+# impulse response plotovi
+ir_plot <- irf(var_mdl, n.ahead = 10, boot = TRUE)
+plot(ir_plot,nc=2, nr=2)
